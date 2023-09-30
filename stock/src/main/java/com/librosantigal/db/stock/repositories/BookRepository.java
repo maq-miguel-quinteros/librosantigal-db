4a5c07b28ab11a368acc79becf625c416376140b
Miguel Ángel Quinteros <57157602+maq-miguel-quinteros@users.noreply.github.com>
2023-09-30 18:31:41 -0300

    Delete stock/src/main/java/com/librosantigal/db/stock/repositories directory

--- stock/src/main/java/com/librosantigal/db/stock/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.librosantigal.db.stock.repositories;
-
-import com.librosantigal.db.stock.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface BookRepository extends JpaRepository <Book, Long>{
-}
